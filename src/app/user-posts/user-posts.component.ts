71888e63768b7b4b59eeccea7a8ef6e5ffe094ab
Nika Kopadze <nika.kopadze.1@btu.edu.ge>
2022-03-22 14:20:52 +0400

    add posts

--- src/app/user-posts/user-posts.component.ts
+++ src/app/user-posts/user-posts.component.ts
@@ -1,15 +1,35 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { Subscription } from 'rxjs';
+import { Post } from '../interfaces/interfaces';
+import { ApiService } from '../services/api.service';
 
 @Component({
   selector: 'app-user-posts',
   templateUrl: './user-posts.component.html',
   styleUrls: ['./user-posts.component.scss']
 })
-export class UserPostsComponent implements OnInit {
+export class UserPostsComponent implements OnInit, OnDestroy {
 
-  constructor() { }
+  public posts !: Post[]
+  public userId !: number
+  private userSubscribtion !: Subscription
+
+  constructor(private apiService: ApiService, private route: ActivatedRoute) { }
+
 
   ngOnInit(): void {
+    this.userId = this.route.snapshot.params["id"]
+    this.posts = []
+
+    // სერვისში აღწერილი ფუნქციის გამოყენებით მომაქვს კონკრეტულ user-ID- სთან დაკავშირებული პოსტები
+    this.userSubscribtion = this.apiService.getAllPostsByUserId(this.userId).subscribe((data) => {
+      this.posts = data
+    })
+  }
+
+  ngOnDestroy(): void {
+    this.userSubscribtion.unsubscribe()
   }
 
 }
