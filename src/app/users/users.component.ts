a23cb2a6b04b2fe3d05dc853402ab134d768517a
Nika Kopadze <nika.kopadze.1@btu.edu.ge>
2022-03-22 13:12:34 +0400

    add users page

--- src/app/users/users.component.ts
+++ src/app/users/users.component.ts
@@ -1,15 +1,33 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { Subscription } from 'rxjs';
+import { User } from '../interfaces/interfaces';
+import { ApiService } from '../services/api.service';
 
 @Component({
   selector: 'app-users',
   templateUrl: './users.component.html',
   styleUrls: ['./users.component.scss']
 })
-export class UsersComponent implements OnInit {
+export class UsersComponent implements OnInit, OnDestroy {
 
-  constructor() { }
+  public allUsers !: User[]
+
+  private getAllUsersSubscription !: Subscription
+
+  constructor(private apiService: ApiService) { }
+
 
   ngOnInit(): void {
+    this.allUsers = []
+
+    // სერვისში აღწერილი ფუნქციის გამოყენებით მომაქვს ყველა მომხმარებელი და ვათავსებ ცვლადში რომელზეც შემდეგ მოხდება ციკლით გადავლა
+    this.getAllUsersSubscription = this.apiService.getAllUsers().subscribe((data: Array<User>) => {
+      this.allUsers = data
+    })
+  }
+
+  ngOnDestroy(): void {
+    this.getAllUsersSubscription.unsubscribe()
   }
 
 }
