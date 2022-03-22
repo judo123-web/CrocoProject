338f33995a0ee96b3acafcaaac05151e3c6daddf
Nika Kopadze <nika.kopadze.1@btu.edu.ge>
2022-03-22 13:16:11 +0400

    add user information

--- src/app/user-information/user-information.component.ts
+++ src/app/user-information/user-information.component.ts
@@ -1,15 +1,31 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { Subscription } from 'rxjs';
+import { User } from '../interfaces/interfaces';
+import { ApiService } from '../services/api.service';
 
 @Component({
   selector: 'app-user-information',
   templateUrl: './user-information.component.html',
   styleUrls: ['./user-information.component.scss']
 })
-export class UserInformationComponent implements OnInit {
+export class UserInformationComponent implements OnInit, OnDestroy {
 
-  constructor() { }
+  public user !: User
+  private userSubscription !: Subscription
+  constructor(private route: ActivatedRoute, private apiService: ApiService) { }
 
   ngOnInit(): void {
+    this.user = {} as User
+
+    // სერვისში აღწერილი ფუნქციის გამოყენებით მომაქვს კონკრეტული მომხმარებელი
+    this.userSubscription = this.apiService.getUserInfoById(this.route.snapshot.params['id']).subscribe((data: User) => {
+      this.user = data
+    })
+  }
+
+  ngOnDestroy(): void {
+    this.userSubscription.unsubscribe()
   }
 
 }
